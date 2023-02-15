cb16be11ce3afa60287242e1674d20ccf6b1a5ab
malika776e <malikaanbazova05@gmail.com>
2023-02-15 14:30:16 +0600

    .

--- src/components/Navbar/Burger.jsx
+++ src/components/Navbar/Burger.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const Burger = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Burger = ({ header, items, active, setActive }) => {
+  return (
+    <div
+      className={active ? "menu active" : "menu"}
+      onClick={() => setActive(false)}
+    >
+      <div className="blur" />
+      <div className="menu__content" onClick={(e) => e.stopPropagation()}>
+        <div className="menu__header">{header}</div>
+        <ul>
+          {items.map((item) => (
+            <li>
+              <a href={item.href}>{item.value}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Burger;
