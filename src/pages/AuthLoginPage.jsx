ac6215c414347002caa9f1075df029333b983d48
aminanaz1~ <aftaami@gmail.com>
2023-02-15 19:18:39 +0600

    added authPage

---
+++ src/pages/AuthLoginPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AuthLogin from "../components/Auth/AuthLogin";
+
+const AuthLoginPage = () => {
+  return (
+    <div>
+      <AuthLogin />
+    </div>
+  );
+};
+
+export default AuthLoginPage;
