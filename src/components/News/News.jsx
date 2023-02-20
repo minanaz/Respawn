c24cc0b08b1601562a9ce74f69bf7e3b8b29c08d
aminanaz1~ <aftaami@gmail.com>
2023-02-20 12:20:04 +0600

    add games and auth

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const News = () => {
-  return <div></div>;
+  let news = [
+    {
+      title: "APEX LEGENDS",
+      description: "Soon bla bla",
+      image:
+        "https://www.respawn.com/media/images/apex-mobile-art-featured-image.jpg.2e16d0ba.fill-594x447.jpg",
+      date: "Jun 3, 2022",
+      id: 1,
+    },
+  ];
+  return (
+    <div>
+      {news.map((item) => (
+        <div></div>
+      ))}
+    </div>
+  );
 };
 
 export default News;
--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,15 +1,14 @@
 import { Typography } from "@mui/material";
 import React from "react";
 import AboutUs from "../components/AboutUs/AboutUs";
-import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 
 const AboutUsPage = () => {
   return (
     <div>
       <Navbar />
+      {/* <Typography sx={{color: "white"}}>AboutUs</Typography> */}
       <AboutUs />
-      <Footer />
     </div>
   );
 };
